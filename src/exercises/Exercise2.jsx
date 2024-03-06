6611b042b439c708c5e6e0bdaaefcec7857860c9
Keg <38766915+kegdotcom@users.noreply.github.com>
2024-03-05 21:36:44 -0600

    Revert "Post meeting"

--- src/exercises/Exercise2.jsx
+++ src/exercises/Exercise2.jsx
@@ -1,37 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const UserPosts = () => {
-  const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState([])
 
-  // API URL (open in your browser to see what the data looks like): https://jsonplaceholder.typicode.com/posts
+	// API URL (open in your browser to see what the data looks like): https://jsonplaceholder.typicode.com/posts
 
-  /* Fetch Usage (for JSON data):
+	/* Fetch Usage (for JSON data):
 		fetch("url").then(res => res.json()).then(res => {
 			// do something with res
 		})
 	*/
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((res) => {
-        setPosts(res);
-      });
-  }, []);
-
-  return (
-    <div>
-      {posts.map((post) => (
-        // TODO: Render each post as a div with an <h3 /> for the post title and a <p /> for the post body
-        <>
-          <div key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </div>
-        </>
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			{
+				posts.map(post => (
+					// TODO: Render each post as a div with an <h3 /> for the post title and a <p /> for the post body
+					<>
+					</>
+				))
+			}
+		</div>
+	);
 };
 
 export default UserPosts;
--- src/exercises/Exercise4.jsx
+++ src/exercises/Exercise4.jsx
@@ -1,49 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const FormValidation = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [invalid, setInvalid] = useState(false);
+	// Add your state and useEffect logic here for a controlled input and your error message
 
-  function handleInput(e) {
-    setInputValue(e.target.value);
-  }
+	// Validate the input's value so that only inputs under 16 characters are valid
 
-  useEffect(() => {
-    if (inputValue.length >= 16) {
-      setInvalid(true);
-    } else {
-      setInvalid(false);
-    }
-  }, [inputValue]);
-
-  // Add your state and useEffect logic here for a controlled input and your error message
-  let hopeful; // undefined at first by default
-  let condition = true;
-  if (condition) {
-    // 'hopeful' is only defined if 'condition' is true
-    hopeful = "we hope we are able to use this string";
-  }
-  const settle = "but if we dont have it we can settle for this";
-
-  // Validate the input's value so that only inputs under 16 characters are valid
-  return (
-    <div>
-      {/* Create the input field and display the error message in a <p> element, but only if there was an error with the validation */}
-      <input
-        type="text"
-        placeholder="Name (max 16 characters)"
-        value={inputValue}
-        onChange={handleInput}
-      />
-      {/* && short circuit: this only renders '<p>Error Message</p> 
-	  if 'invalid' is true, otherwise nothing is rendered*/}
-      {invalid && <p>Error Message</p>}
-      {/* || short circuit: this will return the value of hopeful if defined, 
-	  otherwise we fall back on the value in 'settle'
-	  so that the user doesn't just see nothing */}
-      {hopeful || settle}
-    </div>
-  );
-};
+	return (
+		<div>
+			{/* Create the input field and display the error message in a <p> element, but only if there was an error with the validation */}
+			<input type="text" placeholder="Name (max 16 characters)" />
+			<p>Error Message</p>
+		</div>
+	)
+}
 
 export default FormValidation;
