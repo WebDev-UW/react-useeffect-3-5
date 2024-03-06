79b43c70fb1298953a6120a5c9bffffa29c270a5
kavakian <38766915+kegdotcom@users.noreply.github.com>
2024-03-05 20:07:30 -0600

    added code during meeting

--- src/exercises/Exercise1.jsx
+++ src/exercises/Exercise1.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Logging() {
-	const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-	function incrementCount() {
-		setCount(count + 1)
-	}
+  function incrementCount() {
+    setCount(count + 1);
+  }
 
-	// TODO: Create a useEffect hook that logs something only once on initial render
+  // TODO: Create a useEffect hook that logs something only once on initial render
+  useEffect(() => {
+    console.log("This only logs once");
+  }, []);
 
+  // TODO: Create a useEffect hook that logs something initially and every time the count changes
+  useEffect(() => {
+    console.log("Logs once and on count change");
+  }, [count]);
 
-
-	// TODO: Create a useEffect hook that logs something initially and every time the count changes
-
-
-
-	return (
-		<div>
-			<button onClick={incrementCount}>Increment</button>
-			<p>Count: {count}</p>
-		</div>
-	);
+  return (
+    <div>
+      <button onClick={incrementCount}>Increment</button>
+      <p>Count: {count}</p>
+    </div>
+  );
 }
 
 export default Logging;
--- src/exercises/Exercise3.jsx
+++ src/exercises/Exercise3.jsx
@@ -1,15 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function CountdownTimer() {
-	const [timeRemaining, setTimeRemaining] = useState(10);
+  const [timeRemaining, setTimeRemaining] = useState(10);
 
-	// TODO: use setInterval to decrease the time remaining every 1000 milliseconds (1 second)
+  // TODO: use setInterval to decrease the time remaining every 1000 milliseconds (1 second)
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
 
-	return (
-		<div>
-			<p>Time remaining: {timeRemaining} seconds</p>
-		</div>
-	);
+    let startTime = Date.now();
+    console.log("doing something right now");
+    let endTime = Date.now();
+    console.log(endTime - startTime);
+
+    // teardown function
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timeRemaining]);
+
+  return (
+    <div>
+      <p>Time remaining: {timeRemaining} seconds</p>
+    </div>
+  );
 }
 
 export default CountdownTimer;
